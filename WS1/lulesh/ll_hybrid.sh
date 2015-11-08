e9d8263087e5f2c544f504e1c275f16df8b8d758
IgorTo <igor.tominec@gmail.com>
2015-11-08 17:09:51 +0100

    hybrid

--- WS1/lulesh/ll_hybrid.sh
+++ WS1/lulesh/ll_hybrid.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #@ wall_clock_limit = 00:20:00
-#@ job_name = pos-lulesh-openmp
+#@ job_name = pos-lulesh-hybrid
 #@ job_type = MPICH
 #@ class = test
-#@ output = pos_lulesh_openmp_$(jobid).out
-#@ error = pos_lulesh_openmp_$(jobid).out
+#@ output = pos_lulesh_hybrid_$(jobid).out
+#@ error = pos_lulesh_hybrid_$(jobid).out
 #@ node = 1
 #@ total_tasks = 16
 #@ node_usage = not_shared
@@ -15,6 +15,10 @@
 
 . /etc/profile
 . /etc/profile.d/modules.sh
+. $HOME/.bashrc
 
-export OMP_NUM_THREADS=16 # Correct here for different # of threads.
-./lulesh2.0
+module unload mpi.ibm
+module load mpi.intel
+
+export OMP_NUM_THREADS=2 # Correct here for different # of threads.
+mpiexec -n 8 ./lulesh2.0
